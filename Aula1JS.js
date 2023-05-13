4667a60300813d8a29dfafd7c2b7113e74a94cb5
VictorRod14 <125521237+VictorRod14@users.noreply.github.com>
2023-05-13 12:57:02 -0300

    Add files via upload

---
+++ Aula1JS.js
@@ -0,0 +1,22 @@
+
+let nome = "Paulo Victor Avelar Rodovalho";
+let idade = 35;
+let altura = 1.73;
+let peso = 106;
+let salario = 2500;
+let profissão = "Personal";
+
+let maioridade = 18;
+let imc = (peso / altura**2);
+
+console.log ("Meu nome é", nome);
+console.log ("O seu imc é", imc);
+
+if (idade>=18){
+    console.log ("Você é maior de idade");
+}
+if (salario >=2000){
+    console.log ("Muito bom");
+}
+
+
